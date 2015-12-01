33b66a923fc71c1b61311d542a67c6e1122816c1
Zottán <ferkazoli@gmail.com>
2015-12-01 17:33:47 +0200

    signup bugfix

--- control/signup.php
+++ control/signup.php
@@ -2,19 +2,20 @@
 
 require_once("/model/user.php");
 
-if(!isset($_SESSION['username'])) require_once("views/signup.php");
-else {
-    if (isset($_POST['user']) && isset($_POST['password'])) {
-        $user = $_POST['user'];
-        $password = $_POST['password'];
-        $userData = [
-            'user' => $user,
-            'password' => $password
-        ];
-        if (userAdd($userData)) {
-            echo "Success!";
-        } else {
-            echo "userAdd error!";
-        }
-    } else echo "Please fill all of the fields!";
-}
+if (isset($_POST['user']) && isset($_POST['password'])) {
+    $user = $_POST['user'];
+    $password = $_POST['password'];
+    $userData = [
+        'user' => $user,
+        'password' => $password
+    ];
+    if (userAdd($userData)) {
+        $_SESSION["username"] = $user;
+        $host  = $_SERVER['HTTP_HOST'];
+        $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+        $extra = '';
+        header("Location: http://$host$uri/$extra");
+    } else {
+        echo "userAdd error!";
+    }
+} else require_once("views/signup.php");
